2c4027241690144b67da148c1e2ae32936625487
Rayya12 <152264203+Rayya12@users.noreply.github.com>
2024-03-04 08:59:17 +0700

    Update latihan_part3.go

--- latihan_part3.go
+++ latihan_part3.go
@@ -1,1 +1,31 @@
 
+package main
+
+import "fmt"
+
+func BesCil(a, b *int) {
+	var perantara int
+	if *a > *b {
+		perantara = *a
+		*a = *b
+		*b = perantara
+	}
+
+}
+
+func main() {
+	var m, n, o, p, q, r, s, t int
+	fmt.Scanln(&m, &n)
+	fmt.Scanln(&o, &p)
+	fmt.Scanln(&q, &r)
+	fmt.Scanln(&s, &t)
+
+	BesCil(&m, &n)
+	BesCil(&o, &p)
+	BesCil(&q, &r)
+	BesCil(&s, &t)
+	fmt.Println(m, n)
+	fmt.Println(o, p)
+	fmt.Println(q, r)
+	fmt.Println(s, t)
+}
